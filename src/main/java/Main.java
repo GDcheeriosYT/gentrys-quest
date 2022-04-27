d2a993fe68fca0f66520f80f674b5701c4d22e86
GDcheerios <minecreeper0913@gmail.com>
2022-04-27 02:15:05 -0500

    migrate to source

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,6 @@ import data.Inventory;
 class Main{
   static Inventory inventory = new Inventory();
   public static void main(String[] args){
-    //gacha(false, 1000);
 
     System.out.println(content.characters.getContentCharacters());
 
