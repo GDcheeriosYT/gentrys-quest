c6e7cafa0f073824616f99a1afba113579221231
GDcheerios <minecreeper0913@gmail.com>
2022-05-01 17:25:02 -0500

    added artifact property

--- src/main/java/location/BattleArea.java
+++ src/main/java/location/BattleArea.java
@@ -1,15 +1,19 @@
 package location;
 import java.util.ArrayList;
+
+import artifact.Artifact;
 import enemy.Enemy;
 
 public class BattleArea {
   private final String name;
   private final boolean isBossArea;
   private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+  private ArrayList<Artifact> artifacts = new ArrayList<Artifact>();
   
   public BattleArea(String name, boolean isBossArea, ArrayList<Enemy> enemies){
     this.name = name;
     this.isBossArea = isBossArea;
     this.enemies = enemies;
+    this.artifacts = artifacts;
   }
 }
