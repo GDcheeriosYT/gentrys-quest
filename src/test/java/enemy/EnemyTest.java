ce9a18ea41e7a6499801f1a3212ba8e7f65dfad0
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-09 16:34:35 +0300

    Tested all getters and setters of Enemy class
    Took 14 minutes

--- src/test/java/enemy/EnemyTest.java
+++ src/test/java/enemy/EnemyTest.java
@@ -10,6 +10,7 @@ import weapon.Weapon;
 
 public class EnemyTest {
 
+    private Enemy e;
     private Weapon weapon;
     private String name, description;
     private int health, attack, defense;
@@ -24,12 +25,52 @@ public class EnemyTest {
         this.defense = 10;
         this.weapon = mock(Weapon.class);
         this.description = "Dummy description";
-
+        // Enemy 'e' ONLY initialised here -> reduce code duplication
+        e = new Enemy(this.name, this.health, this.attack, this.defense, weapon, this.description);
     }
 
     @Test
     void constructorTest() {
-        Enemy e = new Enemy("Vlad", 100, 20, 10, weapon, "dummy description");
         assertThat(e).isNotNull();
+    }
+
+    @Test
+    void getHealthTest() {
+        assertThat(e.getHealth()).isEqualTo(1000);
+    }
+
+    @Test
+    void setHealthTest() {
+        e.setHealth(50);
+        assertThat(e.getHealth()).isEqualTo(50);
+    }
+
+    @Test
+    void getLevelTest() {
+        assertThat(e.getLevel()).isEqualTo(1); // Initially all enemies are lvl.1
+    }
+
+    @Test
+    void getNameTest() {
+        assertThat(e.getName()).isEqualTo(name);
+    }
+    @Test
+    void getAttackTest(){
+        assertThat(e.getAttack()).isEqualTo(attack * (int)(1 * 0.9));
+    }
+
+    @Test
+    void getDefenseTest(){
+        assertThat(e.getDefense()).isEqualTo((int)(1 * (int)(1 * 0.85)));
+    }
+
+    @Test
+    void getDescriptionTest(){
+        assertThat(e.getDescription()).isEqualTo(description);
+    }
+
+    @Test
+    void getWeaponTest(){
+        assertThat(e.getWeapon()).isEqualTo(weapon);
     }
 }
