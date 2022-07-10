0fcb1d1ec52290ad2e7d7c804eb46911515584b3
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-10 19:28:53 +0300

    Tested get/remove/add artifacts functionality
    Took 8 minutes

---
+++ src/test/java/inventory/InventoryTest.java
@@ -0,0 +1,44 @@
+package inventory;
+
+import artifact.Artifact;
+import character.Character;
+import data.Inventory;
+import org.junit.jupiter.api.Test;
+import weapon.Weapon;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class InventoryTest {
+    private boolean infiniteMoney;
+    private Artifact artifact = mock(Artifact.class);
+    private Character character = mock(Character.class);
+    private Weapon weapon = mock(Weapon.class);
+    private Inventory inventory = new Inventory();
+
+
+    @Test
+    void defaultConstructorTest() {
+        assertThat(inventory).isNotNull();
+    }
+
+    @Test
+    void addArtifactTest() {
+        inventory.addArtifact(artifact);
+        assertThat(inventory.getArtifacts()).containsExactly(artifact);
+    }
+
+    @Test
+    void getArtifactTesst() {
+        assertThat(inventory.getArtifacts()).isEmpty();
+    }
+
+    @Test
+    void removeArtifactTest() {
+        inventory.addArtifact(artifact);
+        inventory.removeArtifact(artifact);
+        assertThat(inventory.getArtifacts()).isEmpty();
+    }
+}
