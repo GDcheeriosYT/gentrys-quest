928989263cfefe2ceedf18a2a7fd6d7fc97b7915
GDcheerios <minecreeper0913@gmail.com>
2022-07-13 22:58:42 -0500

    changed battle area maxLevel threshold

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -589,7 +589,7 @@ class Main{
     ArrayList<Artifact> artifacts = new ArrayList<Artifact>();
     ArrayList<Enemy> enemies = new ArrayList<Enemy>();
     artifacts = battleArea.initializeArtifacts(character.getDifficulty());
-    enemies = battleArea.initializeEnemies(character.getDifficulty(), (character.getLevel() % 20));
+    enemies = battleArea.initializeEnemies(character.getDifficulty(), (character.getLevel() % 20) + 2);
 
     boolean ran = false;
     String ending = "";
