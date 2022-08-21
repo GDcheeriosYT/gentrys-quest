a4e424718b77a38dc234419fb65413d2f4fe0569
GDcheerios <minecreeper0913@gmail.com>
2022-08-21 05:05:36 -0500

    testing with okhttp and jansi
    I have an idea on where I could implement classes to manage these libraries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,26 @@
 import java.io.*;
 
 import Settings.*;
+
 import org.fusesource.jansi.*;
 import static org.fusesource.jansi.Ansi.ansi;
 import static org.fusesource.jansi.Ansi.Color;
+import okhttp3.*;
 
 class Main{
-
+  OkHttpClient client = new OkHttpClient();
   public static void main(String[] args) throws IOException {
     AnsiConsole.systemInstall();
-    AnsiConsole.out().println( ansi().eraseScreen().render("@|red Hello|@ @|green World of black men|@") );
+    AnsiConsole.out().println( ansi().eraseScreen().render("@|green args[0]|@ @|red args[1]|@ @|blue args[2]|@") );
+  }
+
+  public String run(String url) throws IOException {
+    Request request = new Request.Builder()
+            .url(url)
+            .build();
+
+    try (Response response = client.newCall(request).execute()) {
+      return response.body().string();
+    }
   }
 }
